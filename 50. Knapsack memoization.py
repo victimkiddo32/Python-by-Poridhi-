b16663fd9840ee5e8b9ba2ad5e6434b16caa20ab
victimkiddo32 <arindamchy32@gmail.com>
2025-10-12 23:42:53 +0600

    memoization approach

---
+++ 50. Knapsack memoization.py
@@ -0,0 +1,24 @@
+# git add . && git commit -m "Your commit message" && git push -u origin main
+
+#memoization of 0/1 knapsack
+def f(i,capacity,w,v,memo):
+    if i<0 or capacity==0:
+        return 0
+    if (i,capacity) in memo:
+        return memo[(i,capacity)]
+    not_take=f(i-1,capacity,w,v,memo)
+    take=-1
+    if w[i]<=capacity:
+        take=v[i]+f(i-1,capacity-w[i],w,v,memo)
+    
+    memo[(i,capacity)]=max(take,not_take)
+    return memo[(i,capacity)]
+
+if __name__ == "__main__":
+    weight=[3,2,5]
+    value=[30,40,60]
+    capacity=6
+    n=len(value)
+    memo={}
+    ans=f(n-1,capacity,weight,value,memo)
+    print(ans)
