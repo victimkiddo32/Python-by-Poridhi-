f902d41ecb3945c1e6653996cc9becb1176d8e8a
victimkiddo32 <arindamchy32@gmail.com>
2025-10-14 20:46:08 +0600

    Dp problem

--- 54.Decode ways ( recursion ) .py
+++ 54.Decode ways ( recursion ) .py
@@ -1,5 +1,8 @@
 #git add . ; git commit -m "Dp problem" ; git push -u origin main
 
+#TC: o(2^n)
+#SC: o(n) for recursion stack
+
 def f(s, n):
     if n == 0:
         return 1
@@ -12,7 +15,7 @@ def f(s, n):
 
     # take 2 digits (if valid)
     two_digit = 0
-    if n >= 2 and 10 <= int(s[:2]) <= 26:#check if first two digts are valid
+    if n >= 2 and 10 <= int(s[:2]) <= 26: #check if first two digts are valid
         two_digit = f(s[2:], n - 2)
 
     return one_digit + two_digit
